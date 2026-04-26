6d4b2fab90172f509f61fbd05291ca01ed821562
你的名字 <574754058@qq.com>
2026-04-26 15:39:40 +0800

    Auto sync:2026/04/26 周日 15:39:40.00

--- OpenCursor.bat
+++ OpenCursor.bat
@@ -1,12 +1,2 @@
-setlocal
-
-set "PROJECT_DIR=%~dp0"
-
-rem Prefer the GUI exe to avoid extra console windows
-if exist "%LocalAppData%\Programs\Cursor\Cursor.exe" (
-  start "" "%LocalAppData%\Programs\Cursor\Cursor.exe" "%PROJECT_DIR%" >nul 2>nul
-) else (
-  start "" /b cursor "%PROJECT_DIR%" >nul 2>nul
-)
-
-exit /b 0
+cursor .
+exit
