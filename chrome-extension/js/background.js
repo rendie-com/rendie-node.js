9b1c395d4c35893068909e593f44002c2faad401
你的名字 <574754058@qq.com>
2026-05-14 11:23:29 +0800

    Auto sync:2026/05/14 周四 11:23:29.18

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -1,3 +1,118 @@
+//background.js
 'use strict';
-import { index } from './index.js';
+import { common } from './common/index.js';
+import { proxy } from './common/proxy.js';
+import { common_fetch } from './common/fetch.js';
+import { common_cookies } from './common/cookies.js';
+import { common_tabs } from './common/tabs.js';
+import { common_uploadFile } from './common/uploadFile.js';
+export const index =
+{
+    screenshot: "",//截图的图片，暂时存放在这。
+    a01: function () {
+        let This = this;
+        chrome.runtime.onMessage.addListener(function (request, sender, next) {
+            if (request.cmd == 'content-script-rendie-com')//表是只接收【content-script】的信息。
+            {
+                //console.log('【rendie/js/index.js】收到消息', request);
+                switch (request.id) {
+                    case "load_screenshot": next(This.screenshot); break;
+                    case "close_screenshot_window": This.c01(next); break;
+                    case "isOpenDevtools": common_tabs.isOpenDevtools = true; next(); break;//是否已打开Devtools页面。
+                    case "isOnRequestFinished": common_tabs.isOnRequestFinished = true; next(); break;//已打开监听器。
+                    default: This.d01(request, next); break;
+                }
+            }
+            return true;//注：不写这个会出错，内容为【Unchecked runtime.lastError: The message port closed before a response was received.】
+        });
+        this.a02(This)
+    },
+    a02: function (This) {
+        chrome.action.onClicked.addListener(function (tab) {
+            //console.log("扩展图标被点击了!");
+            chrome.tabs.captureVisibleTab({
+                format: "png"
+            }, function (t) {
+                This.screenshot = t;
+                This.a03()
+            })
+        });
+    },
+    a03: function () {
+        chrome.tabs.create({
+            url: chrome.runtime.getURL("screenshot/screenshot.html")
+        })
+    },
+    /////////////////////////
+    c01: function (next) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.remove(tabs[0].id, function () {
+                next()
+            });
+        });
+    },
+    /////////////////////////////////////////////
+    d01: function (request, next) {
+        if (request.action == "isRD") {
+            // 标签页ID，{active: true, currentWindow: true}参数确保查询的是当前窗口中活动的标签
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                next(tabs[0].windowId)
+            });//获取当前窗体【windowId】
+        }
+        else {
+            if (request.windowId != 0) {
+                this.d02(request, next)
+            } else {
+                next("谷歌扩展中【windowId】未初始化，请先运行“gg.isRD”方法。")
+                //ommon.notifications("未初始化", "谷歌扩展中【windowId】未初始化，请先运行“gg.isRD”方法。", chrome.runtime.getURL('icons/icon256.png'), next);
+            }
+        }
+    },
+    d02: function (request, next) {
+        switch (request.action) {
+            /////////////js/common/tabs.js 开始////////////////////////////////
+            case "tabs_remove_create_indexOf"://删除后创建再查找(返回网页内容)
+                common_tabs.tabs_remove_create_indexOf.a01(request.index, request.windowId, request.url, request.htmlArr, request.isHighlightTab, next);
+                break;
+            case "tabs_remove_create_devtools_indexOf"://删除后创建再查找(返回网页内容)  --- 要打开“开发者工具“ 
+                common_tabs.tabs_remove_create_devtools_indexOf.a01(request.index, request.windowId, request.url, request.htmlArr, request.isHighlightTab, next);
+                break;
+            case "tabs_executeScript_indexOf"://执行js代码后再找内容(返回网页内容)
+                common_tabs.tabs_executeScript_indexOf.a01(request.index, request.windowId, request.fileArr, request.code, request.htmlArr, request.isHighlightTab, next);
+                break;
+            case "tabs_executeScript_devtools_indexOf"://执行js代码后再找内容(返回网页内容) --- 要打开“开发者工具“ 
+                common_tabs.tabs_executeScript_devtools_indexOf.a01(request.index, request.windowId, request.fileArr, request.code, request.htmlArr, request.isHighlightTab, next);
+                break;
+            //case "tabs_executeScript": //执行js代码(立马返回值取到的内容) 
+            //     Tool.tabs_executeScript.a01(request.index, request.windowId, request.fileArr, request.code, request.isHighlightTab, next);
+            //     break;
+            ///////////js/common/tabs.js 结束/////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////////////
+            ///////////common_cookies.js  开始//////////////////////////////////////////////////
+            case "delAllCookies": common_cookies.delAllCookies(request.urlArr, next); break;//删除所有cookie信息
+            case "setAllCookies": common_cookies.setAllCookies(request.cookiesArr, next); break;//设置所有cookie信息
+            case "getAllCookies": common_cookies.getAllCookies(request.urlArr, [], next); break;//获取所有cookie信息
+            case "getCookies": common_cookies.getCookies(request.url, request.name, request.partitionKey, next); break;//获取单个cookie信息----(注：这里多了一个【partitionKey】，有的不加这个获取不到值)
+            // //case "setCookies": Tool.setCookies(request); break;//设置单个cookie信息
+            case "delCookies": common_cookies.delCookies(request.url, request.name, next); break;//删除单个cookie信息
+            // //////////common_cookies.js  结束//////////////////////////////////////////////////////////
+            // ////////////////////////////////////////////////////////
+            case "getFetch": common_fetch.getFetch(request.url, request.type, next); break;//获取URL内容
+            case "getHeadersFetch": common_fetch.getHeadersFetch(request.url, request.headersObj, request.type, next); break;//获取URL内容
+            case "postHeadersFetch": common_fetch.postHeadersFetch(request.url, request.headersObj, request.data, request.type, next); break;//获取URL内容
+            case "postFetch": common_fetch.postFetch(request.url, request.data, next); break;//普通提交（返回的内容不一定是json格式
+            case "typeFetch": common_fetch.typeFetch(request.url, request.type, request.data, next); break;//指定类型提交
+            // case "download": Tool.download(request.url, request.filename, next); break; //下载文件---------已验证
+            case "uploadFile": common_uploadFile.uploadFile.a01(request.url, request.type, request.headers, request.data, next); break;//上传文件------已验证
+            case "highlightTab": common.highlightTab.a01(request.index, request.windowId, next); break;//高亮tab------已验证
+            // case "notifications": Tool.notifications(request.title, request.message, request.iconUrl, next); break;//通知------已验证
+            case "enableProxy": proxy.enable(request.pacScript, next); break;//开启代理
+            case "disableProxy": proxy.disable(next); break;//关闭代理
+            default: next({
+                status: "error",
+                data: "在【background.js】找不到方法名：" + request.action
+            });
+        }
+    },
+}
 index.a01();
--- chrome-extension/js/content-script.js
+++ chrome-extension/js/content-script.js
@@ -1,37 +1,40 @@
 ﻿'use strict';
-let fun = {
-    a01: function () {
-        var This = this;
-        window.addEventListener("message", function (e)//接收消息
-        {
-            //console.log('【rendie/js/content-script.js】收到消息：', e.data);
-            if (e.data.cmd == 'content-script-rendie-com') {
-                chrome.runtime.sendMessage(e.data, function (r) {
-                    if (chrome.runtime.lastError) {
-                        // 在这里捕捉错误，防止它直接弹出到控制台
-                        console.debug("忽略预料中的连接错误");
-                    }
-                    else {
-                        This.Return(r);
-                    }
-                });
-            }
-        }, false);
-        this.loadJs()
-    },
-    ////////////////////////////////////////////////////////
-    Return: function (t) {
-        let data = {
-            return: t,
-            cmd: "return-rendie-com"
+
+(function () {
+    window.addEventListener("message", function (e) {
+        // 1. 过滤消息
+        if (!e.data || e.data.cmd !== 'content-script-rendie-com') return;
+
+        // 2. 检查插件环境是否已失效
+        if (!chrome.runtime || !chrome.runtime.id) {
+            // 环境失效，直接回传错误给前端 gg.Return，不报控制台错误
+            window.postMessage({
+                cmd: "return-rendie-com",
+                return: { status: "error", msg: "extension_context_invalidated" }
+            }, '*');
+            return;
         }
-        window.postMessage(data, '*')
-    },
-    loadJs: function ()//加载JS文件
-    {
-        var temp = document.createElement('script');
-        temp.setAttribute('type', 'text/javascript');
-        temp.src = chrome.runtime.getURL("inject/index.js");
-        document.head.appendChild(temp);
-    },
-}.a01();
+
+        try {
+            // 3. 业务转发：所有 action（包括 isRD）都统一发送给 background
+            chrome.runtime.sendMessage(e.data, function (r) {
+                if (chrome.runtime.lastError) {
+                    // 后台通信异常，回传错误状态
+                    window.postMessage({
+                        cmd: "return-rendie-com",
+                        return: { status: "error", msg: chrome.runtime.lastError.message }
+                    }, '*');
+                } else {
+                    // 正常返回数据给网页端的 gg.Return
+                    window.postMessage({ return: r, cmd: "return-rendie-com" }, '*');
+                }
+            });
+        } catch (err) {
+            // 捕获同步异常（如发送瞬间插件刚好被关闭），回传错误
+            window.postMessage({
+                cmd: "return-rendie-com",
+                return: { status: "error", msg: err.message }
+            }, '*');
+        }
+    }, false);
+})();
